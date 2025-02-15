5b4a49919602b4c9fd2daa26a8dae7133823a382
Mykola Tarasenko <nikolajtarasenko71@gmail.com>
2025-02-15 17:26:50 +0200

    Add env variables

--- app/settings.py
+++ app/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,11 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-tpp$l_uqubd%mlz_1q#exf+34h$2@27%&4vrpqtk4pa$cv!$y("
+# SECURITY WARNING: keep the secret key used in production secret!4
+load_dotenv()
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-tpp$l_uqubd%mlz_1q#exf+34h$2@27%&4vrpqtk4pa$cv!$y(")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "True")
 
 ALLOWED_HOSTS = []
 
